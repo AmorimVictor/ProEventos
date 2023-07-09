de4c62d11bb0fc4e70433f1a7df6119ba3bec5f6
AmorimVictor <victor.amorim21@hotmail.com>
2023-07-09 17:47:42 -0300

    Correção table Eventos

--- Back/src/ProEventos.Domain/Evento.cs
+++ Back/src/ProEventos.Domain/Evento.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProEventos.Domain
-{
-    [Table("EventosDetalhes")]
+{    
     public class Evento
     {
         public int Id { get; set; }
